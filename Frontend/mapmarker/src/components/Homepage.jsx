d4fd68ba26ebbbbf9343b6d651da906c1f27a44c
Nirajan Banjade <nirajanbanjade321@gmail.com>
2024-12-23 15:13:52 -0600

    Initial commit

---
+++ Frontend/mapmarker/src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Worrying about your car in parking?</h1>
+      <p>Say no more!</p>
+      <p> Track your car as you go along!</p>
+    </div>
+  );
+};
+
+export default Homepage;
